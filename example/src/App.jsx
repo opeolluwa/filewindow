c01c7db4178bd28d80269a98a24602f586e5d8e9
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-07-02 00:34:28 +0100

    experimatal:impl image and audio player

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,12 +1,15 @@
 import FileWindow from "../../src/FileWindow/FileWindow";
 import "./App.css";
-import testImage from "../../test-files/test-image.png";
-import testVideo from "../../test-files/test-video.mp4";
+import testImage from "../test-files/test-image.png";
+import testVideo from "../test-files/test-video.mp4";
+import testAudio from "../test-files/test-audio.mp3";
+
 function App() {
   return (
     <>
-      {/* <FileWindow fileUrl={testImage} fileType="image" className="" /> */}
+      <FileWindow fileUrl={testImage} fileType="image" className="" />
       <FileWindow fileUrl={testVideo} fileType="video" className="" />
+      <FileWindow fileUrl={testAudio} fileType="audio" className="" />
     </>
   );
 }
